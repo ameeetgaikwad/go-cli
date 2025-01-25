86d71855ffe72a11d7981aa605fb9d0e02abd49a
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2025-01-25 10:11:33 +0530

    init-base cli

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	var language string
+	app := &cli.App{
+		Name:  "greet",
+		Usage: "fight the loneliness!",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "lang",
+				Value:       "english",
+				Usage:       "language for the greeting",
+				Destination: &language,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			name := "amit"
+
+			if cCtx.NArg() > 0 {
+				name = cCtx.Args().Get(0)
+			}
+			// if cCtx.String("lang") == "spanish" {
+			// 	fmt.Println("Hola", name)
+
+			// }
+			if language == "spanish" {
+				fmt.Println("Hola", name)
+
+			} else {
+				fmt.Println("Hello", name)
+			}
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
